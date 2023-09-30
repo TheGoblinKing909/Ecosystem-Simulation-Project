9ffcb42f1aa897faf2f03d46ba0ea683b51539b9
TheGoblinKing909 <bryceolesen@mail.com>
2023-09-30 18:41:28 -0500

    Deleted multiple assets

--- Ecosystem Simulation Project/Assets/Scripts/TileGeneration.cs
+++ Ecosystem Simulation Project/Assets/Scripts/TileGeneration.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class TileGeneration : MonoBehaviour
-{
-    public float scale = .1f;
-    public int size = 100;
+public class TileGeneration : MonoBehaviour {
+
+    public float scale;
+    public int size;
     public Tilemap tilemap = null;
     public List<Tile> tileList = new List<Tile>();
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+
+        scale = .1f;
+        size = 150;
+
         float[,] noiseMap = new float[size, size];
         (float xOffset, float yOffset) = (Random.Range(-10000f, 10000f), Random.Range(-10000f, 10000f));
         for(int y = 0; y < size; y++) {
@@ -27,19 +30,20 @@ public class TileGeneration : MonoBehaviour
                 float noiseHeight = noiseMap[x, y];
                 float colorHeight = noiseHeight * tileList.Count;
                 int colorIndex = Mathf.FloorToInt(colorHeight);
-                if (colorIndex == tileList.Count)
-                {
-                    colorIndex = tileList.Count-1;
+                if (colorIndex == tileList.Count) {
+                    colorIndex = tileList.Count - 1;
                 }
-                float tileHeight = noiseHeight * tileList.Count * 2;
-                int tileHeightIndex = Mathf.FloorToInt(tileHeight);
-                // tileHeightIndex -= 4;
-                // if (tileHeightIndex < 0)
-                // {
-                //     tileHeightIndex = 0;
-                // }
+                if (colorIndex == -1) {
+                    colorIndex = 0;
+                }
+                float tileHeight = noiseHeight * tileList.Count;
+                int tileHeightIndex = Mathf.FloorToInt(tileHeight) * 2;
+                tileHeightIndex -= 4;
+                if (tileHeightIndex < 0) {
+                    tileHeightIndex = 0;
+                }
                 Tile tile = tileList[colorIndex];
-                Vector3Int p = new Vector3Int(x - size / 2, y - size / 2, tileHeightIndex);
+                Vector3Int p = new Vector3Int(x - size / 2 + tileHeightIndex / 2, y - size / 2 + tileHeightIndex / 2, tileHeightIndex);
                 tilemap.SetTile(p, tile);
             }
         }
