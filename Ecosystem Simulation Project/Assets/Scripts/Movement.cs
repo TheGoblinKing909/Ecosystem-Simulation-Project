24dae6849865d76cd980268c907f7d3b5c682dcb
ColbyCherry <colby_cherry@yahoo.com>
2023-10-19 16:19:02 -0500

    Simple Movement Between Tilemaps
    Adjusted movement script for basic movement between tilemaps based on collision

--- Ecosystem Simulation Project/Assets/Scripts/Movement.cs
+++ Ecosystem Simulation Project/Assets/Scripts/Movement.cs
@@ -8,8 +8,8 @@ public class TestMovement : MonoBehaviour
 
     float horizontal;
     float vertical;
-
-    public float runSpeed = 20.0f;
+    public float runSpeed = 5.0f;
+    int collisionCount;
 
     // Start is called before the first frame update
     void Start()
@@ -27,5 +27,26 @@ public class TestMovement : MonoBehaviour
     private void FixedUpdate()
     {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        collisionCount = 0;
+        Debug.Log("collision entered");
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        collisionCount++;
+        Debug.Log("collision count = " + collisionCount);
+        if (collisionCount == 50)
+        {
+            transform.position = transform.position + new Vector3(horizontal / 2, vertical / 2, 0);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("collision exited");
     }
 }
