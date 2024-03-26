66489b4f8c0d1a26c74652b02f5a3eb34ba2113c
archestarchie <103324524+archestarchie@users.noreply.github.com>
2024-03-26 12:57:57 -0500

    March 26th, 2024
    unfinished menu lol

--- Ecosystem Simulation Project/Assets/Scripts/MainMenuController.cs
+++ Ecosystem Simulation Project/Assets/Scripts/MainMenuController.cs
@@ -8,8 +8,16 @@ public class MainMenuController : MonoBehaviour
 {
     public CanvasGroup OptionPanel, WorldParam;
     public TMPro.TMP_InputField userWidth, userHeight, userSeed, userOctaves, userScale, userPersistence, userLacunarity, userResDensity, userEntDensity;
+    public Slider sliderWidth, sliderHeight, sliderSeed, sliderOctaves, sliderScale, sliderPersistance, sliderLacunarity, sliderResDensity, sliderEntDensity;
     public static int inputWidth, inputHeight, inputSeed, inputOctaves;
     public static float inputScale, inputPersistence, inputLacunarity, inputResDensity, inputEntDensity;
+
+
+    public void Update()
+    {
+        sliderWidth.onValueChanged.AddListener(OnSliderChanged(userWidth, sliderWidth));
+        userWidth.onValueChanged.AddListener(OnFieldChanged(userWidth, sliderWidth));
+    }
 
     public void StartGame()
     {
@@ -24,6 +32,26 @@ public class MainMenuController : MonoBehaviour
         inputEntDensity = float.Parse(userEntDensity.text);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void OnSliderChanged(TMPro.TMP_InputField input, Slider slider)
+    {
+        if (input.text != slider.value.ToString())
+        {
+            input.text = slider.value.ToString();
+        }
+    }
+
+    public void OnFieldChanged(TMPro.TMP_InputField input, Slider slider)
+    {
+        if (slider.value.ToString() != input.text)
+        {
+            if (float.TryParse(input.text, out float number))
+            {
+                slider.value = number;
+            }
+        }
+    }
+
     public void Create()
     {
         WorldParam.alpha = 1;
