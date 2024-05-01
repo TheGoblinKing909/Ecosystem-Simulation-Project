e29cb9fe5a95f4c1e9d640642267d92c478f6f15
PeliRax <pelirax@proton.me>
2024-05-01 17:44:10 -0500

    updated rewards and observations

---
+++ Ecosystem Simulation Project/Assets/Water.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+
+    public ResourceType ResourceType = ResourceType.Water;
+
+}
